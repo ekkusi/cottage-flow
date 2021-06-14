996a98f8cf89d496c3a221084173cca3ddcf689d
Ekku Sipilä <ekku.eki@gmail.com>
2021-06-14 19:00:58 +0300

    Copy gatsby-browser.ts to ssr

--- frontend/gatsby-ssr.tsx
+++ frontend/gatsby-ssr.tsx
@@ -1,3 +1,11 @@
+/**
+ * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
+ *
+ * See: https://www.gatsbyjs.com/docs/ssr-apis/
+ */
+
+// You can delete this file if you're not using it
+
 import React, { useState } from "react";
 import { AnimatePresence } from "framer-motion";
 import { WrapPageElementNodeArgs } from "gatsby";
