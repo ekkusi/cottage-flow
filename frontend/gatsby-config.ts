b9a090b89ba50813520f5fb5b42a2ddba9ca25c9
Ekku Sipilä <ekku.sipila@bonsky.com>
2021-06-15 12:20:05 +0300

    Fix Chakra modal staying under Loader

--- frontend/gatsby-config.ts
+++ frontend/gatsby-config.ts
@@ -29,7 +29,22 @@ module.exports = {
       },
     },
     `gatsby-plugin-gatsby-cloud`,
-    `@chakra-ui/gatsby-plugin`,
+    {
+      resolve: "@chakra-ui/gatsby-plugin",
+      options: {
+        /**
+         * @property {boolean} [resetCSS=true]
+         * if `false`, this plugin will not use `<CSSReset />
+         */
+        resetCSS: true,
+        /**
+         * @property {number} [portalZIndex=40]
+         * The z-index to apply to all portal nodes. This is useful
+         * if your app uses a lot z-index to position elements.
+         */
+        portalZIndex: 10000000,
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
